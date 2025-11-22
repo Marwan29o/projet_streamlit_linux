9ae1cca68813120774b38a0635ef59f248ce5ac5
Hamzaoui <marwan@MacBook-Pro-de-Marwan.local>
2025-11-22 18:30:26 +0100

    update get_data.sh

--- data_collector/bin/get_data.sh
+++ data_collector/bin/get_data.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 source data_collector/conf/collector.conf
-
 echo "Form ID is set as: ${GDRIVE_FILE_ID}"
 echo "Target path is set as: ${path}"
-
-curl -L "https://drive.google.com/uc?export=download&id=${GDRIVE_FILE_ID}" \
-  -o "${path}/data.csv"
+curl -L "https://drive.google.com/uc?export=download&id=${GDRIVE_FILE_ID}" \-o "${path}/data.csv"
