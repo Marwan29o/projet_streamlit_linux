6217a5962ff2f07f71154e10b79bb2cf76aa6e9a
Hamzaoui <marwan@MacBook-Pro-de-Marwan.local>
2025-12-05 17:58:50 +0100

    Ajout d'un dossier data pour stockage des données lors du lancement de l'app

--- data_collector/bin/get_data.sh
+++ data_collector/bin/get_data.sh
@@ -2,4 +2,5 @@
 source data_collector/conf/collector.conf
 echo "Form ID is set as: ${GDRIVE_FILE_ID}"
 echo "Target path is set as: ${path}"
+mkdir -p data
 curl -L "https://drive.google.com/uc?export=download&id=${GDRIVE_FILE_ID}" \-o "${path}/data.csv"
